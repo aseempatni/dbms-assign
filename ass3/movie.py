64262804c4e45c8e7c805a73568e90345442623f
Aseem Patni <apatnip@gmail.com>
2015-02-01 15:40:51 +0530

    added dob data and cleaned some files

--- ass3/movie.py
+++ ass3/movie.py
@@ -4,8 +4,11 @@ import warnings
 import pprint as pp
 import sys
 
-# import parse function to get movie data
+# this script adds the data to the database
+
+# import parse function to get movie data and dob of person
 from parse import getMovie
+from parse import getDob
 
 # connect to database and get a cursor
 try:
@@ -20,6 +23,10 @@ except mdb.Error, e:
 f = open ("input.txt")
 lines = f.readlines()
 prefix = lines[1]
+
+# config
+addDob = False
+
 
 # add to movie table
 def add_movie(data) :
@@ -39,15 +46,25 @@ def add_movie(data) :
 
 def add_cast(data):
     for i in range(len(data['cast'])):
-
-        # add cast to person
-        sql = ""
-        sql = sql + "INSERT INTO " +prefix+"_person(PID, name) "
-        sql = sql + "SELECT * FROM (SELECT " + "\""+data['cast'][i][0]+"\",\""+data['cast'][i][1]+"\"" +") as tmp "
-        sql = sql + "WHERE NOT EXISTS ( SELECT PID FROM  " + prefix+"_person WHERE PID =\"" + data['cast'][i][0] + "\" ) LIMIT 1"
-        # print sql
-        cursor.execute(sql)
-        db.commit()
+        if addDob==True:
+            dob = getDob(data['cast'][i][0])
+            # add cast to person
+            sql = ""
+            sql = sql + "INSERT INTO " +prefix+"_person(PID, name, DOB) "
+            sql = sql + "SELECT * FROM (SELECT " + "\""+data['cast'][i][0]+"\",\""+data['cast'][i][1]+"\"" +", "+ dob +") as tmp "
+            sql = sql + "WHERE NOT EXISTS ( SELECT PID FROM  " + prefix+"_person WHERE PID =\"" + data['cast'][i][0] + "\" ) LIMIT 1"
+            # print sql
+            cursor.execute(sql)
+            db.commit()
+        else:
+            # add cast to person
+            sql = ""
+            sql = sql + "INSERT INTO " +prefix+"_person(PID, name) "
+            sql = sql + "SELECT * FROM (SELECT " + "\""+data['cast'][i][0]+"\",\""+data['cast'][i][1]+"\"" +") as tmp "
+            sql = sql + "WHERE NOT EXISTS ( SELECT PID FROM  " + prefix+"_person WHERE PID =\"" + data['cast'][i][0] + "\" ) LIMIT 1"
+            # print sql
+            cursor.execute(sql)
+            db.commit()
 
         # add the mapping
         sql = ""
@@ -58,6 +75,125 @@ def add_cast(data):
         cursor.execute(sql)
         db.commit()
 
+def add_producer(data):
+    for i in range(len(data['producer'])):
+
+        # add producer to person
+        sql = ""
+        sql = sql + "INSERT INTO " +prefix+"_person(PID, name) "
+        sql = sql + "SELECT * FROM (SELECT " + "\""+data['producer'][i][0]+"\",\""+data['producer'][i][1]+"\"" +") as tmp "
+        sql = sql + "WHERE NOT EXISTS ( SELECT PID FROM  " + prefix+"_person WHERE PID =\"" + data['producer'][i][0] + "\" ) LIMIT 1"
+        # print sql
+        cursor.execute(sql)
+        db.commit()
+
+        # add the mapping
+        sql = ""
+        sql = sql + "INSERT INTO " +prefix+"_m_producer(MID, PID) "
+        sql = sql + "SELECT * FROM (SELECT " + "\""+data['MID']+"\",\""+data['producer'][i][0]+"\"" +") as tmp "
+        sql = sql + "WHERE NOT EXISTS ( SELECT MID, PID FROM  " + prefix+"_m_producer WHERE MID =\"" + data['MID'] + "\" AND PID =\"" + data['producer'][i][0] + "\" ) LIMIT 1"
+        # print sql
+        cursor.execute(sql)
+        db.commit()
+
+def add_director (data):
+    # add director to map and person
+    sql = ""
+    sql = sql + "INSERT INTO " +prefix+"_person(PID, name) "
+    sql = sql + "SELECT * FROM (SELECT " + "\""+data['DID']+"\",\""+data['director']+"\"" +") as tmp "
+    sql = sql + "WHERE NOT EXISTS ( SELECT PID FROM  " + prefix+"_person WHERE PID =\"" + data['DID']+ "\" ) LIMIT 1"
+    cursor.execute(sql)
+    db.commit()    
+    sql = ""
+    sql = sql + "INSERT INTO " +prefix+"_m_director(MID, PID) "
+    sql = sql + "SELECT * FROM (SELECT " + "\""+data['MID']+"\",\""+data['DID']+"\"" +") as tmp "
+    sql = sql + "WHERE NOT EXISTS ( SELECT MID, PID FROM  " + prefix+"_m_director WHERE MID =\"" + data['MID'] + "\" AND PID =\"" + data['DID'] + "\" ) LIMIT 1"
+    cursor.execute(sql)
+    db.commit()    
+
+def add_genre (data):
+    for i in range(len(data['genre'])):
+        # add genre to genre and m_genre
+        sql = ""
+        sql = sql + "INSERT INTO " +prefix+"_genre(name) "
+        sql = sql + "SELECT * FROM (SELECT " + "\""+data['genre'][i]+"\") as tmp "
+        sql = sql + "WHERE NOT EXISTS ( SELECT name FROM  " + prefix+"_genre WHERE name =\"" + data['genre'][i]+ "\" ) LIMIT 1"
+        cursor.execute(sql)
+        db.commit()
+        sql = "SELECT GID FROM "+prefix+"_genre" + " WHERE name = \"" + data['genre'][i] + "\""
+        cursor.execute(sql)
+        GID = str(cursor.fetchall()[0][0])
+        # print GID
+        sql = ""
+        sql = sql + "INSERT INTO " +prefix+"_m_genre(MID, GID) "
+        sql = sql + "SELECT * FROM (SELECT " + "\""+data['MID']+"\","+ GID +") as tmp "
+        sql = sql + "WHERE NOT EXISTS ( SELECT MID, GID FROM  " + prefix+"_m_genre WHERE MID =\"" + data['MID'] + "\" AND GID =" + GID + " ) LIMIT 1"
+        # print sql
+        cursor.execute(sql)
+        db.commit() 
+
+def add_country (data):
+    for i in range(len(data['country'])):
+        # add country to country and m_country
+        sql = ""
+        sql = sql + "INSERT INTO " +prefix+"_country(name) "
+        sql = sql + "SELECT * FROM (SELECT " + "\""+data['country'][i]+"\") as tmp "
+        sql = sql + "WHERE NOT EXISTS ( SELECT name FROM  " + prefix+"_country WHERE name =\"" + data['country'][i]+ "\" ) LIMIT 1"
+        cursor.execute(sql)
+        db.commit()
+        sql = "SELECT CID FROM "+prefix+"_country" + " WHERE name = \"" + data['country'][i] + "\""
+        cursor.execute(sql)
+        CID = str(cursor.fetchall()[0][0])
+        # print CID
+        sql = ""
+        sql = sql + "INSERT INTO " +prefix+"_m_country(MID, CID) "
+        sql = sql + "SELECT * FROM (SELECT " + "\""+data['MID']+"\","+ CID +") as tmp "
+        sql = sql + "WHERE NOT EXISTS ( SELECT MID, CID FROM  " + prefix+"_m_country WHERE MID =\"" + data['MID'] + "\" AND CID =" + CID + " ) LIMIT 1"
+        # print sql
+        cursor.execute(sql)
+        db.commit() 
+
+def add_language (data):
+    for i in range(len(data['language'])):
+        # add language to language and m_language
+        sql = ""
+        sql = sql + "INSERT INTO " +prefix+"_language(name) "
+        sql = sql + "SELECT * FROM (SELECT " + "\""+data['language'][i]+"\") as tmp "
+        sql = sql + "WHERE NOT EXISTS ( SELECT name FROM  " + prefix+"_language WHERE name =\"" + data['language'][i]+ "\" ) LIMIT 1"
+        cursor.execute(sql)
+        db.commit()
+        sql = "SELECT LAID FROM "+prefix+"_language" + " WHERE name = \"" + data['language'][i] + "\""
+        cursor.execute(sql)
+        LAID = str(cursor.fetchall()[0][0])
+        # print LAID
+        sql = ""
+        sql = sql + "INSERT INTO " +prefix+"_m_language(MID, LAID) "
+        sql = sql + "SELECT * FROM (SELECT " + "\""+data['MID']+"\","+ LAID +") as tmp "
+        sql = sql + "WHERE NOT EXISTS ( SELECT MID, LAID FROM  " + prefix+"_m_language WHERE MID =\"" + data['MID'] + "\" AND LAID =" + LAID + " ) LIMIT 1"
+        # print sql
+        cursor.execute(sql)
+        db.commit() 
+
+def add_location (data):
+    for i in range(len(data['location'])):
+        # add location to location and m_location
+        sql = ""
+        sql = sql + "INSERT INTO " +prefix+"_location(name) "
+        sql = sql + "SELECT * FROM (SELECT " + "\""+data['location'][i]+"\") as tmp "
+        sql = sql + "WHERE NOT EXISTS ( SELECT name FROM  " + prefix+"_location WHERE name =\"" + data['location'][i]+ "\" ) LIMIT 1"
+        cursor.execute(sql)
+        db.commit()
+        sql = "SELECT LID FROM "+prefix+"_location" + " WHERE name = \"" + data['location'][i] + "\""
+        cursor.execute(sql)
+        LID = str(cursor.fetchall()[0][0])
+        # print LID
+        sql = ""
+        sql = sql + "INSERT INTO " +prefix+"_m_location(MID, LID) "
+        sql = sql + "SELECT * FROM (SELECT " + "\""+data['MID']+"\","+ LID +") as tmp "
+        sql = sql + "WHERE NOT EXISTS ( SELECT MID, LID FROM  " + prefix+"_m_location WHERE MID =\"" + data['MID'] + "\" AND LID =" + LID + " ) LIMIT 1"
+        # print sql
+        cursor.execute(sql)
+        db.commit() 
 
 def ptable(name):
     sql = "select * from "+prefix+ "_" + name
@@ -68,23 +204,20 @@ def ptable(name):
 
 # add data for movie with id to db
 def add_all(id):
-
-    # with warnings.catch_warnings():
-    #     warnings.simplefilter('ignore')
-    #     sql = "DROP TABLE IF EXISTS "+prefix+"_m_producer"
-    #     print sql
-    #     cursor.execute(sql)
-    #     sql = "DROP TABLE IF EXISTS "+prefix+"_m_director"
-
     # get movie data
     print "Getting data"
     data = getMovie(id)
     # pp.pprint (data)
+
     print "Adding "+data['title'] +" to db"
     add_movie(data)
-    # ptable("movie")
     add_cast(data)
-
+    add_director(data)
+    add_genre(data)
+    add_country(data)
+    add_language(data)
+    add_location(data)
+    add_producer(data)
 
 if __name__ == '__main__':
     id = sys.argv[1]
--- ass3/parse.py
+++ ass3/parse.py
@@ -4,13 +4,16 @@ import lxml.html
 import pprint
 import numpy as np
 
-#sudo pip install requests==1.2.3 lxml==3.2.1
+# This script scraps the data from the imdb web pages
 
+#sudo pip install requests==1.2.3 lxml==3.2.1
 
 def getMovie(id):
     hxs = lxml.html.document_fromstring(requests.get("http://www.imdb.com/title/" + id).content)
     movie = {}
+
     movie['MID'] = id
+
     try:
         movie['title'] = hxs.xpath('//*[@id="overview-top"]/h1/span[1]/text()')[0].strip()
     except IndexError:
@@ -30,22 +33,19 @@ def getMovie(id):
         movie['num_votes'] = hxs.xpath('//*[@id="overview-top"]/div[3]/div[3]/a[1]/span/text()')[0].strip()
     except IndexError:
         movie['votes'] = ""
-    
+
     try:
         movie['genre'] = hxs.xpath('//*[@itemprop="genre"]/a/text()')
     except IndexError:
         movie['genre'] = []
-
     try:
         movie['country'] = hxs.xpath('//*[@id="titleDetails"]/div/h4[contains(text(),"Country")]/ancestor::div[1]/a/text()')
     except IndexError:
         movie['country'] = ""
-
     try:
         movie['language'] = hxs.xpath('//*[@id="titleDetails"]/div/h4[contains(text(),"Language")]/ancestor::div[1]/a/text()')
     except IndexError:
         movie['language'] = ""
-
     try:
         movie['location'] = hxs.xpath('//*[@id="titleDetails"]/div/h4[contains(text(),"Location")]/ancestor::div[1]/a/text()')
     except IndexError:
@@ -79,9 +79,13 @@ def getMovie(id):
     except IndexError:
         movie['director'] = ""
     try:
-        movie['stars'] = hxs.xpath('//*[@id="overview-top"]/div[6]/a/span/text()')
+        movie['DID'] = hxs.xpath('//*[@id="overview-top"]/div[4]/a/@href')[0].strip()[6:15]
     except IndexError:
-        movie['stars'] = ""
+        movie['DID'] = ""
+    # try:
+    #     movie['stars'] = hxs.xpath('//*[@id="overview-top"]/div[6]/a/span/text()')
+    # except IndexError:
+    #     movie['stars'] = ""
     # try:
     #     movie['poster'] = hxs.xpath('//*[@id="img_primary"]/div/a/img/@src')[0]
     # except IndexError:
@@ -95,9 +99,13 @@ def getMovie(id):
     # except IndexError:
     #     movie['storyline'] = ""
 
+    return add_cast_details(movie,id)
 
-    hxs = lxml.html.document_fromstring(requests.get("http://www.imdb.com/title/" + id + "/fullcredits").content)
     
+def add_cast_details (movie,id):
+    hxs = lxml.html.document_fromstring(requests.get("http://www.imdb.com/title/" + id + "/fullcredits").content)
+
+    # cast
     try:
         movie['cast'] = hxs.xpath('//*[@itemprop="actor"]/a/span/text()')
     except IndexError:
@@ -110,7 +118,31 @@ def getMovie(id):
         cast_id[i] = cast_id[i][6:15]
     cast =  np.column_stack([cast_id, movie['cast']])
     movie['cast'] = cast
+
+    # producer
+    try:
+        movie['producer'] = hxs.xpath('//*[@id="fullcredits_content"]/table[4]/tbody[1]/tr/td[1]/a/text()')
+        producer_id = hxs.xpath('//*[@id="fullcredits_content"]/table[4]/tbody[1]/tr/td[1]/a/@href')
+    except IndexError:
+        movie['producer'] = ""   
+    for i in range(len(producer_id)):
+        producer_id[i] = producer_id[i][6:15]
+        movie['producer'][i] = movie['producer'][i].strip()
+    # print movie["producer"]
+    producer = np.column_stack([producer_id,movie["producer"]])
+    movie["producer"] = producer
+
     return movie
+
+
+def getDob(id):
+    hxs = lxml.html.document_fromstring(requests.get("http://www.imdb.com/name/" + id).content)
+    try:
+        date = hxs.xpath('//*[@itemprop="birthDate"]/@datetime')[0]
+    except IndexError:
+        date = ""
+    return date
+
 
 if __name__ == "__main__":
         id = sys.argv[1]
